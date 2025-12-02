b488668864264d8d64a2be08bdf25b70732d05b3
emelielundvall <luem25zf@student.ju.se>
2025-12-02 12:16:31 +0100

    changes

--- game.js
+++ game.js
@@ -1,3 +1,10 @@
+import { character } from "./character.js";
+import platform from "platform";
 
+function setup() {
+    createCanvas(400, 600);
+}
 
-
+if(character.y + character.h < 450) {
+    character.y += 10;
+}
