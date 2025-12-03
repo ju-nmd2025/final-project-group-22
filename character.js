217a26bb1122a7b39207d982b6239b87112847da
emelielundvall <luem25zf@student.ju.se>
2025-12-03 14:41:59 +0100

    Update character.js

--- character.js
+++ character.js
@@ -1,20 +1,35 @@
-function drawCharacter(x, y) {
-    fill(255, 204, 0);
-    circle(x, y, 300);
-    
-    fill(80, 60, 40);
-    circle(x-50, y-35, 50);
-    
-    fill(80, 60, 40);
-    circle(x+50, y-35, 50); 
-    
-    fill(80, 60, 40);
-    arc(x, y+30, 170, 140, 0, PI);
+function setup (){
+    createCanvas (500, 500);
+
+    drawEmoji (250, 250);
 }
 
-let x = 200;
-let y = 200;
-console.log(drawCharacter(x, y));
+function drawEmoji (x, y){
+ fill (255, 255, 90);
+stroke (1);
+strokeWeight (5);
+ellipse (x, y, 250, 250);
+
+fill ("white");
+ellipse (310, 220, 100, 90);
+
+fill ("black");
+ellipse (330, 220, 30, 30);
 
 
 
+ fill("white");
+  ellipse(140, 250, 120, 60); 
+
+   fill("white");
+  ellipse(140, 280, 140, 60); 
+
+  
+
+  fill("orange");
+  ellipse(350, 290, 100, 40);
+
+  fill("red");
+  ellipse(350, 290, 100, 10);
+  
+}
