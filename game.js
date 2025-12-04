3b82e626451ca48eba6c7c618dcbd168520fbbaa
emelielundvall <luem25zf@student.ju.se>
2025-12-04 21:47:49 +0100

    Update game.js

--- game.js
+++ game.js
@@ -3,6 +3,90 @@ import platform from "platform";
 
 function setup() {
     createCanvas(400, 600);
+<<<<<<< Updated upstream
+=======
+    textSize(18);
+    textStyle(BOLD);
+
+    // create platforms
+    for (let i = 0; i < 12; i++) {
+      platforms.push(new Platform(Math.random() * (width - 60), height - i * 40)
+      );
+    }
+
+  // create player
+     player = {
+      x: width / 2,
+      y: height - 100,
+      w: 30,
+      h: 30,
+      vy: 0
+     };
+  }
+
+  function draw() {
+    if (gameState === "start") {
+      drawStartScreen();
+    } else if (gameState === "play") {
+      drawGame();
+    }
+  }
+   
+  function drawStartScreen() {
+    background(135, 206, 235); // Sky blue
+
+    if(mouseX > 150 && mouseX <235 && mouseY > 200 && mouseY <235){
+        fill(0, 0, 0);
+        cursor(HAND);
+    } else {
+        fill(231, 84, 128);
+        cursor(ARROW);
+    }
+  
+    rect(150, 200, 105, 35, 5);
+    fill("white");
+    text("Start game", 157, 222);
+  }
+
+function drawGame() {
+  background(135, 206, 235); // Sky blue
+
+  player.vy += 0.6;
+  player.y += player.vy;
+
+  let scrollSpeed = 0;
+  if (player.y < height / 2 && player.vy < 0) {
+    scrollSpeed = 4;
+  }
+
+  for (let p of platforms) {
+    p.y += scrollSpeed;
+  }
+
+  rect(player.x, player.y, player.w, player.h);
+
+  generatePlatforms(platforms, player.y, width, height);
+
+  for (let p of platforms) {
+    if (
+      player.vy > 0 &&
+      player.x + player.w > p.x &&
+      player.x < p.x + p.w &&
+      player.y + player.h > p.y &&
+      player.y + player.h < p.y + p.h
+    ) {
+      player.vy = -12;
+    }
+  }
+
+  if (player.y > height) {
+    player.y = height - 100;
+    player.vy = 0;
+  }
+
+  if (keyIsDown(LEFT_ARROW)) player.x -= 5;
+  if (keyIsDown(RIGHT_ARROW)) player.x += 5;
+>>>>>>> Stashed changes
 }
 
 if(character.y + character.h < 450) {
