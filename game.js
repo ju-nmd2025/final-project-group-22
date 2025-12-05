c4107e7759895a04f70fe6ce4d4319313ad09aa4
emelielundvall <luem25zf@student.ju.se>
2025-12-05 15:09:27 +0100

    Update game.js

--- game.js
+++ game.js
@@ -101,27 +101,23 @@ function mouseClicked() {
     gameState = "play";
     cursor(ARROW);
   }
-}
-if(character.y + character.h < 450) {
-    character.y += 10;
-}
 
 
-function showGameOver () {
-fill(0, 0, 0, 180);
-rect(0, 0, 400, 600);
-fill(255);
-textSize(35);
-textAlign (CENTER);
-text("GAME OVER", 200, 260);
+  function showGameOver () {
+   fill(0, 0, 0, 180);
+   rect(0, 0, 400, 600);
+   fill(255);
+   textSize(35);
+   textAlign (CENTER);
+   text("GAME OVER", 200, 260);
 
-textSize(15);
-text("Tryck på R för att spela igen!", 200, 320);
+   textSize(15);
+   text("Tryck på R för att spela igen!", 200, 320);
+  }
 
-}
 
-
-function restartGame() {
+  function restartGame() {
     isGameOver = false;
     createPlatforms();
+  }
 }
