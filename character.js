7d92a72bfb05069d10239e6a5b72d6b4692e5cef
emelielundvall <luem25zf@student.ju.se>
2025-12-07 23:27:17 +0100

    Update character.js

--- character.js
+++ character.js
@@ -1,48 +1,39 @@
+export function Character(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+// bird
 function drawCharacter(x, y) {
-    fill(255, 204, 0);
-    circle(x, y, 90);
-    
-    fill(80, 60, 40);
-    circle(x-20, y-5, 25);
-    
-    fill(80, 60, 40);
-    circle(x+20, y-5, 25); 
-    
-    fill(80, 60, 40);
-    arc(x, y+20, 50, 30, 0, PI);
-}
-function setup (){
-    createCanvas (400, 600);
-
-    drawEmoji (250, 250);
+  drawCharacterHead(x, y);
+  drawCharacterEyes(x, y);
+  drawCharacterMouth(x, y);
 }
 
-function drawEmoji (x, y){
- fill (255, 255, 90);
-stroke (1);
-strokeWeight (1);
-ellipse (x, y, 50, 50);
+function drawCharacterHead(x, y) {
+  fill(255, 255, 90);
+  stroke(1);
+  strokeWeight(1);
+  ellipse(x, y, 50, 50);
+}
 
-fill ("white");
-ellipse (x + 10, y - 5, 20, 20);
+function drawCharacterEyes(x, y) {
+  // eyes
+  fill("white");
+  ellipse(x + 10, y - 5, 20, 20);
+  fill("black");
+  ellipse(x + 12, y - 5, 5, 5);
 
-fill ("black");
-ellipse (x + 12, y- 5, 5, 5);
+  //wings
+  fill("white");
+  ellipse(x - 25, y + 5, 30, 20);
+  ellipse(x - 25, y - 5, 20, 10);
+}
 
-
-
- fill("white");
-  ellipse(x - 25, y + 5 , 30, 20); 
-
-   fill("white");
-  ellipse(x- 25, y - 5, 20, 10); 
-
-  
-
+function drawCharacterMouth(x, y) {
   fill("orange");
   ellipse(x + 20, y + 10, 20, 10);
 
   fill("red");
   ellipse(x + 20, y + 10, 20, 1);
-  
 }
